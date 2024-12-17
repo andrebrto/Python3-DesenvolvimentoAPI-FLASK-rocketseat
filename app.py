b3f63baf394023cef8b0f3b9c15664e101503109
André de Brito <andrebcorreia1@gmail.com>
2024-12-17 19:46:22 -0300

    Created models and methods POST

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
 
-@app.route("/about")
-def about():
-    return "Pagina Sobre"
+tasks = []
+control_task_id = 1
+@app.route("/tasks", methods=['POST'])
+def create_task():
+    global control_task_id
+    data = request.get_json()
+    new_task = Task(id=control_task_id, title=data['title'], description=data['description'])
+    control_task_id +1
+    tasks.append(new_task)
+    print(tasks)
+    return "TEST API"
 
 if __name__ == "__main__":
     app.run(debug=True)
