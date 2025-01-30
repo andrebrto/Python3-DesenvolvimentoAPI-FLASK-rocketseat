c1c5d801f3e7fd48fd825597213c8d4444cbe25a
andrebrto <andrebcorreia1@gmail.com>
2025-01-30 01:46:01 -0300

    Performing tests with pytest

--- teste.py
+++ teste.py
@@ -33,19 +33,39 @@ def test_get_task():
         assert response.status_code == 200
         response_json = response.json()
         assert task_id == response_json["id"]
+
 def test_update_task():
     if tasks:
         task_id = tasks[0]
         pay_load = {
-                "title": "Editar minha task",
-                "description": "Editando a task agora",
-                "completed": False
-        }
+            "title": "MEU TESTE COM PUT",
+            "description": "FAZENDO O UPDATE COM PUT",
+            "completed": "COMPLETED",
+        } 
 
-        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=pay_load)
+        response = requests.put(f'{BASE_URL}/tasks/{task_id}', json=pay_load)
         assert response.status_code == 200
         response_json = response.json()
         assert "message" in response_json
 
+        #fazendo a validação do dados do meu update
+        response = requests.get(f'{BASE_URL}/tasks/{task_id}')
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["title"] == pay_load["title"]
+        assert response_json["description"] == pay_load["description"]
+        assert response_json["completed"] == pay_load["completed"]
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f'{BASE_URL}/tasks/{task_id}')
+
+        assert response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404
 
         
